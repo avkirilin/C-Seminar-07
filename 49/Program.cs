7ff0eec0b3172bbdd01f7072c895b62df1305527
Alexander Kirilin <nerosashok@mail.ru>
2023-01-26 14:31:39 +0300

    Решены задачи 7 семинара по C#

---
+++ 49/Program.cs
@@ -0,0 +1,67 @@
+﻿//Задайте двумерный массив. Найдите элементы, у которых оба индекса четные, и замените эти элементы на их квадраты.
+//Например изначально массив выглядел так:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Новый будет выглядеть так:
+// 1 4 49 2
+// 5 9  2 3
+//64 4  4 4
+
+
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)                      //метод получения двумерного массива с рандомными значениями
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < columns; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+int[,] NewArray(int[,] array, int rows, int columns)                                    //метод получения двумерного массива с возведением в квадрат 
+{                                                                                       //четных значений исходного массива
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < columns; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0) result[i, j] = array[i, j] * array[i, j];
+            else result[i, j] = array[i, j];
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)                                                         //"красивый" метод вывода двумерного массива с корректными отступами
+{
+    Console.WriteLine();
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (inArray[i, j] >= 0 && inArray[i, j] < 10) Console.Write($"     {inArray[i, j]}");
+            if (inArray[i, j] >= 10 && inArray[i, j] < 100) Console.Write($"    {inArray[i, j]}");
+            if (inArray[i, j] >= 100 && inArray[i, j] < 1000) Console.Write($"   {inArray[i, j]}");
+            if (inArray[i, j] >= 1000 && inArray[i, j] < 10000) Console.Write($"  {inArray[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.WriteLine("Введите кол-во строк и кол-во столбцов массива через пробел");
+string[] f = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int rowsArray = int.Parse(f[0]);
+int columnsArray = int.Parse(f[1]);
+Console.WriteLine("Введите минимальное и максимальное значения элементов массива в диапазоне 0-99, через пробел");
+string[] d = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int minNum = int.Parse(d[0]);
+int maxNum = int.Parse(d[1]);
+int[,] myArray = GetArray(rowsArray, columnsArray, minNum, maxNum);
+PrintArray(myArray);
+int[,] myEditedArray = NewArray(myArray, rowsArray, columnsArray);
+PrintArray(myEditedArray);
---
+++ 51/Program.cs
@@ -0,0 +1,63 @@
+﻿//Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1,1) и т.д.)
+//Например задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Сумма элементов главной диаагонали 1 + 9 + 2 = 12
+
+
+
+
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < columns; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+int GetSumNum(int[,] array, int rows, int columns)
+{
+    int sumValue = 0;
+    for (int i = 0; i < columns; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            if (i == j) sumValue = sumValue + array[i, j];
+        }
+    }
+    return sumValue;
+}
+
+void PrintArray(int[,] inArray)                                                         //"красивый" метод вывода двумерного массива с корректными отступами
+{
+    Console.WriteLine();
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (inArray[i, j] >= 0 && inArray[i, j] < 10) Console.Write($"     {inArray[i, j]}");
+            if (inArray[i, j] >= 10 && inArray[i, j] < 100) Console.Write($"    {inArray[i, j]}");
+            if (inArray[i, j] >= 100 && inArray[i, j] < 1000) Console.Write($"   {inArray[i, j]}");
+            if (inArray[i, j] >= 1000 && inArray[i, j] < 10000) Console.Write($"  {inArray[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите кол-во строк и кол-во столбцов массива через пробел");
+string[] f = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int rowsArray = int.Parse(f[0]);
+int columnsArray = int.Parse(f[1]);
+Console.WriteLine("Введите минимальное и максимальное значения элементов массива в диапазоне 0-9999, через пробел");
+string[] d = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int minNum = int.Parse(d[0]);
+int maxNum = int.Parse(d[1]);
+int[,] myArray = GetArray(rowsArray, columnsArray, minNum, maxNum);
+PrintArray(myArray);
+Console.WriteLine();
+Console.WriteLine($"Сумма значений массива, находящихся на главной диагонали равно: {GetSumNum(myArray, rowsArray, columnsArray)}");
